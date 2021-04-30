10d7287deb9b19da4ce64b1c2d74506995a08180
Raisan <info@riyblog.shop>
2021-04-30 21:23:41 +0900

    Create code.js

---
+++ code.js
@@ -0,0 +1,3 @@
+document.head.appendChild(document.createElement("link")).setAttribute("rel", "https://code-raisan.github.io/MarinDeck_Extension/main.css");
+
+//document.body.appendChild(document.createElement("script")).setAttribute("src", "https://0e0.pw/gZ0L");
